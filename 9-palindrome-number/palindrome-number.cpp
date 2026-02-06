5442454e936baac67113cbe85a1b089fcc3a55da
Ayush Jaipuriyar <ayushjaipuriyar21@gmail.com>
2026-02-06 13:56:47 +0530

    Time: 0 ms (100.00%) | Memory: 5.9 MB (100.00%) - LeetCode Ultimate

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        int r,temp = x;
+        double sum = 0;
+        while(x>0){
+            r = x%10;
+            sum = sum*10 + r;
+            x = x/10;
+        }
+        if(temp == sum )
+            return true;
+        else
+            return false;
+    }
+};
