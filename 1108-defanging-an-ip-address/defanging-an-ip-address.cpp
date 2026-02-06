fc1fbc3f820ab94e1d345ddac483cf24b3d74b6c
Ayush Jaipuriyar <ayushjaipuriyar21@gmail.com>
2026-02-06 13:55:32 +0530

    Time: 3 ms (25.97%) | Memory: 5.8 MB (100.00%) - LeetCode Ultimate

---
+++ 1108-defanging-an-ip-address/defanging-an-ip-address.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        string ans="";
+        for(int i =0 ;i<address.length();i++){
+            if(address[i]!='.')
+                ans.push_back(address[i]);
+            else
+                ans.append("[.]");
+        }
+        return ans;
+    }
+};
